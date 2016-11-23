151daca9470c1b9c003042a7b52ae9331a57d48b
Friedrice228 <maxkwon@Maxs-MacBook-Pro.local>
2016-11-22 21:08:48 -0800

    Try 2

--- src/GC.java
+++ src/GC.java
@@ -1,78 +1,32 @@
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class GC {
-
-		
-	public static void main(String[] args){
-		
-		int numDNA;
-		
-		Scanner readInput = new Scanner(System.in);
-		
-		System.out.println("How many DNA strings?: ");
-		
-		numDNA = readInput.nextInt();
-		
-		String[] names = new String[numDNA];
-		String[] dna = new String[numDNA];
-		double[] contents = new double[numDNA];
-		
+public class GCp {
 	
-		for (int i = 0; i < numDNA; i++){
-			System.out.println("Name " + i + ": ");
-			names[i] = readInput.nextLine();
-		}
-
-		for (int i = 0; i < numDNA; i++){
-			System.out.println("Name " + i + ": ");
-			names[i] = readInput.nextLine();
-		}
+	public static void main (String args){
 		
-		for (int i = 0; i < numDNA; i++){
-			System.out.println("DNA " + i + ": ");
-			dna[i] = readInput.nextLine();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Data: ");
+		
+		String data = input.nextLine();
+		
+		
+		
+	}
+	
+	public static ArrayList<String> splitSec(String str){
+		
+		ArrayList<String> data = new ArrayList<String>();
+		
+		int trace = 0;
+		
+		for (int i = 0; i < str.length(); i ++){			
+			if ()
 			
-			contents[i] = getContentGC(dna[i]);
-		}
-	
-		//System.out.println(names[determine(contents)] +"  " + contents[determine(contents)]);
-		System.out.println(contents[0] + " " + contents[1]);
-		
-	}
-	
-	public static double getContentGC(String DNA){
-	
-		double gcCounter = 0;
-		for (int i = 0; i < DNA.length(); i++){
-			if (DNA.substring(i, i+1).equals("G") || DNA.substring(i, i+1).equals("C")){
-				gcCounter++;
-			}
 		}
 		
-		double percent = (gcCounter/DNA.length()) * 100.0;
-		
-		return percent;
-		
 	}
-	
-	public static int determine(double[] contents){
-		
-		int maxIndex = 0;
-		double max = 0;
-		for(int i = 0; i < contents.length; i++){
-			if (contents[i] > max){
-				maxIndex = i;
-				max = contents[i];
-				System.out.print("k");
-			}
-		}
-		
-		return maxIndex;
-		
-	}
-	
-	
-	
-	
+
 }
