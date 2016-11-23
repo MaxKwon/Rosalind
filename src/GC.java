8a81491b14d41fc7490d5c8651218a13445d1424
Friedrice228 <maxkwonusa@yahoo.com>
2016-11-23 00:16:36 -0800

    GC

--- src/GC.java
+++ src/GC.java
@@ -4,39 +4,117 @@ import java.util.Scanner;
 
 public class GC {
 	
-	public static void main (String args){
+	 public static void main(String[] args){
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Data: ");
+		//System.out.println("Num data: ");
 		
-		String data = input.nextLine();
+		int numData = 1;
 		
-		System.out.println(splitSec(data) + "k");
+		String[] name = new String[numData];
+		
+		String[] raw = new String[numData];
+		
+		double[] data = new double[numData];
+		
+		for (int i = 0; i < numData; i++){
+			
+			System.out.println("Name" + i + ": ");
+			
+			name[i] = input.nextLine();
+			
+			System.out.println("\r");
+			
+		}
+		
+		for (int i = 0; i < numData; i++){
+			
+			System.out.println("Data" + i + ": ");
+		
+			raw[i] = input.nextLine();
+			
+			System.out.println("\r");
+			
+		}
+		
+		for(int i = 0; i < numData; i++){
+			
+			data[i] = calcPer(raw[i]);
+			
+		}
+		
+		
+		int index = determine(data);
+			
+		System.out.println(name[index] + " " + data[index]);
 		
 		
 	}
-	
-	public static ArrayList<String> splitSec(String str){
+	 
+	public static int findLength(String str){
 		
-		ArrayList<String> data = new ArrayList<String>();
+		int i = 0;
+		while(str.substring(i, i+1).equals(" ")){
+			i++;
+		}
 		
-		int trace = 0;
-		
-		for (int i = 0; i < str.length(); i ++){	
+		int counter = 0;
+		while(!str.substring(i,i+1).equals(" ")){
 			
-			if (str.substring(i, i+1).equals(">") && i != 0){
+			i++;
+			
+			counter++;
+			
+		}
+		
+		return counter;
+		
+	}
+	public static double calcPer(String DNA){
+		
+		int counter = 0;
+		
+		for (int i = 0; i < DNA.length(); i ++){
+			
+			if (DNA.substring(i,  i+1).equals("G") || DNA.substring(i,  i+1).equals("C")){
 				
-				data.add(str.substring(trace, i));
-				
-				trace = i;
+				counter++;
 				
 			}
 			
 		}
 		
-		return data;
+		System.out.println("LENGTH: " + DNA.length());
+		System.out.println("Count: " + counter);
+		
+		double percent = ((double)counter)/((double)DNA.length());
+		
+		return percent;
 		
 	}
+	
+	public static int determine(double[] data){
+		
+		double max = 0;
+		int maxI = 0;
+		
+		for (int i = 0; i < data.length; i++){
+			
+			if (data[i] > max){
+				
+				max = data[i];
+				
+				maxI = i;
+				
+			}
+			
+		}
+		
+		return maxI;
+		
+	}
+	
+	
 
 }
